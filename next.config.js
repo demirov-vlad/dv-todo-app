20d8cee3b6890989fd6bdb99e3c0f539d20657ec
vdemirov <demirov.vlad@gmail.com>
2023-09-27 12:46:33 +0800

    UI created, database created, and DnD functionality implemented

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ["iili.io"],
+  },
+  reactStrictMode: false,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
